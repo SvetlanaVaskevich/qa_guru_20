42847b69e95d4295e5eba11ace82dbf33be4101c
Svetlana Vaskevich <svaskevich@bk.ru>
2022-03-18 10:28:48 +0500

    Allure steps added

--- src/test/java/tests/AndroidSelenideTests.java
+++ src/test/java/tests/AndroidSelenideTests.java
@@ -7,13 +7,17 @@ import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
 import static com.codeborne.selenide.Selectors.byClassName;
 import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
 
 public class AndroidSelenideTests extends TestBase{
 
     @Test
     void searchTest(){
-        $(MobileBy.AccessibilityId("Search Wikipedia")).click();
-        $(MobileBy.id("org.wikipedia.alpha:id/search_src_text")).setValue("BrowserStack");
-        $$(byClassName("android.widget.TextView")).shouldHave(sizeGreaterThan(0));
+        step("Type search",() -> {
+            $(MobileBy.AccessibilityId("Search Wikipedia")).click();
+            $(MobileBy.id("org.wikipedia.alpha:id/search_src_text")).setValue("BrowserStack");
+        });
+        step("Verify content found",() ->
+        $$(byClassName("android.widget.TextView")).shouldHave(sizeGreaterThan(0)));
     }
 }
